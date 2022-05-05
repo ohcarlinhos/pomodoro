98fc8c98d50b776bdc37719485bccf5c2649e3fc
Carlos Roberto <crpu4a@gmail.com>
2022-05-05 10:31:31 -0300

    Atualização do tema global

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,2 +1,15 @@
-export const theme = {};
+export interface ThemeType {
+  colors: {
+    textColor: string;
+    primaryColor: string;
+  };
+}
+
+export const theme: ThemeType = {
+  colors: {
+    textColor: "#2c3e50",
+    primaryColor: "#605bff",
+  },
+};
+
 export const provide = { theme };
