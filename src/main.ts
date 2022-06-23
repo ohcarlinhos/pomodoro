03dd19ce9d145f3a53eaf68bce70f329327919d0
Carlos Roberto <crpu4a@gmail.com>
2022-06-23 20:54:53 -0300

    Configuração do VueGtag

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import VueGtag from "vue-gtag";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +9,14 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(
+  VueGtag,
+  {
+    config: {
+      id: "G-M9CECX79MT",
+    },
+  },
+  router
+);
 
 app.mount("#app");
