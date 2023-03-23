b9e92637f4bf60c9af844e299882556b636f704d
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-03-23 15:25:59 -0300

    conclude language selector and add en i8n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,12 @@
 import { createI18n } from "vue-i18n";
 import ptBr from "./locales/ptBr.json";
+import en from "./locales/en.json";
 
 export const i18n = createI18n({
   locale: "pt-BR",
   legacy: false,
   messages: {
     "pt-BR": ptBr,
+    en: en,
   },
 });
