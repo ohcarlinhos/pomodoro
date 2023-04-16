99ffc808cb2579b74ff2553e586e201f67057aa9
Carlos Roberto <carlos.junior@dotgroup.com.br>
2023-04-16 11:25:19 -0300

    fix: update condicional

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL || "";
+export const API_URL = import.meta.env.VITE_API_URL ?? "";
 export const USE_PANEL = import.meta.env.VITE_USE_PANEL === "true";
-export const GTAG = import.meta.env.VITE_GTAG || "";
+export const GTAG = import.meta.env.VITE_GTAG ?? "";
